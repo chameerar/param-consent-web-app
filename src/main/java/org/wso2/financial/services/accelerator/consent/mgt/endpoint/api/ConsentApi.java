bb2a556bfcf9aa983652a3ac4a66cb95f5355396
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-02-20 13:48:21 +0530

    pom update

--- src/main/java/org/wso2/financial/services/accelerator/consent/mgt/endpoint/api/ConsentApi.java
+++ src/main/java/org/wso2/financial/services/accelerator/consent/mgt/endpoint/api/ConsentApi.java
@@ -1,8 +1,6 @@
 package org.wso2.financial.services.accelerator.consent.mgt.endpoint.api;
 
 //import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 //import org.json.JSONObject;
 //import org.wso2.financial.services.accelerator.common.exception.ConsentManagementException;
 //import org.wso2.financial.services.accelerator.consent.mgt.dao.models.ConsentResource;
@@ -42,7 +40,7 @@ import javax.ws.rs.core.UriInfo;
 public class ConsentApi {
 
 
-    private static final Log log = LogFactory.getLog(ConsentApi.class);
+//    private static final Log log = LogFactory.getLog(ConsentApi.class);
 
     public ConsentApi() {
 
@@ -65,7 +63,7 @@ public class ConsentApi {
             return Response.ok(request.getPathInfo()).build();
 
         } catch (Exception e) {
-            log.error("Error occurred while searching consent data", e);
+//            log.error("Error occurred while searching consent data", e);
             return Response.ok(e).build();
         }
 
